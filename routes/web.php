eda9cb9b5995e04a4b26a8aa715ec6e91739f710
UchMan <uchm4n@gmail.com>
2025-04-22 00:56:54 +0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,9 @@ Route::middleware(['auth','verified'])->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-	Route::post('/text-to-speech', [Dashboard::class, 'textToSpeech'])->name('dashboard.text-to-speech');
 });
+
+Route::post('/text-to-speech', [Dashboard::class, 'textToSpeech'])->name('dashboard.text-to-speech');
 
 
 Route::get('/streaming', function () {
